fe6293b1f9ae6316dfa623ce01c03d5ac5d581dd
Patrício Alex <46056058+patricioalex@users.noreply.github.com>
2025-04-09 16:54:14 -0300

    usando data Provider
    Aplciando Data Provider nos testes

--- tests/Service/CommentTest.php
+++ tests/Service/CommentTest.php
@@ -6,30 +6,47 @@ namespace App\Comment\Tests\Service;
 use App\Model\Comment;
 use App\Model\User;
 use App\Model\News;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class CommentTest extends TestCase
 {
-    public function testCheckArrayComment(){
-        $news = new News('Temas: Vender um item');
-        $user_one = new User('Alex');
-        $user_two = new User('Lucas');
 
-        $news->add_comment(new Comment($user_one, 'Quero compra esse item!'));
-        $news->add_comment(new Comment($user_two, 'Estou pensando em comprar.'));
-
-        self::assertNotEmpty($news, 'deu certo');
+    #[DataProvider('deliverUsersAndComments')]
+    public function testCheckArrayComment(News $news): void
+    {
+        self::assertNotEmpty($news, 'Result is empty');
     }
 
-    public function testQuantosComentários(){
-        $news = new News('Temas: Novo teste');
-        $user_one = new User('Rany');
-        $news->add_comment(new Comment($user_one, 'Quero compra esse item!'));
-        $news->add_comment(new Comment($user_one, 'Estou pensando em comprar.'));
-        $news->add_comment(new Comment($user_one, 'Estou pensando em comprar mais um.'));
-        self::assertCount(3,$news->get_comments(), 'A quantidade não é maior ou inferior ao esperado!');
-        self::assertArrayHasKey(0,$news->get_comments(), 'Chave não existe no array');
-        self::assertArrayHasKey(1,$news->get_comments(), 'Chave não existe no array');
-        self::assertArrayHasKey(2,$news->get_comments(), 'Chave não existe no array');
+    // public function testQuantosComentários(News $news): void
+    // {
+    //     self::assertArrayHasKey(0,$news->get_comments(), 'Chave não existe no array');
+    //     // self::assertArrayHasKey(1,$news->get_comments(), 'Chave não existe no array');
+    //     // self::assertArrayHasKey(2,$news->get_comments(), 'Chave não existe no array');
+    // }
+
+    public static function userOne(){
+        $news = new News('user number one ');
+        $news->add_comment(new Comment(new User('Alex'), 'Hi, my name is Alex'));
+        return [
+            $news
+        ];
+    }
+
+    public static function userTwo(){
+        $news = new News('User number two');
+        $news->add_comment(new Comment(new User('Lucas'), 'Hey, how are you!'));
+        return [
+            $news
+        ];
+    }
+
+
+    public static function deliverUsersAndComments(): array
+    {
+        return [
+            self::userOne(),
+            self::userTwo()
+        ];
     }
 }
